a80030615a43fcef4809e9e896521287ab1f6b12
George Ogden <george-ogden@outlook.com>
2025-11-13 19:51:12 +0000

    Move decorator to correct test

--- test_samples/ignored_test.py
+++ test_samples/ignored_test.py
@@ -1,13 +1,5 @@
-from typing import Callable
 import pytest
 
 
 @pytest.mark.parametrize("unknown", [])
 def ignored() -> None: ...
-
-
-def wrap(x: Callable) -> None: ...
-
-
-@pytest.mark.parametrize("x", [])
-def test_x(x): ...
