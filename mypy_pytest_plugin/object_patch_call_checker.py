2c4ce7acc23dbb54e0b5a605d9c51372cd9b4e61
George Ogden <george-ogden@outlook.com>
2025-11-23 21:26:17 +0000

    Write ObjectPatchCallChecker._attribute_type

--- mypy_pytest_plugin/object_patch_call_checker.py
+++ mypy_pytest_plugin/object_patch_call_checker.py
@@ -1,13 +1,8 @@
 from dataclasses import dataclass
 
 from mypy.checker import TypeChecker
-from mypy.nodes import (
-    CallExpr,
-    Expression,
-)
-from mypy.types import (
-    Type,
-)
+from mypy.nodes import CallExpr, Expression, MemberExpr
+from mypy.types import AnyType, Type
 
 from .argmapper import ArgMapper
 from .patch_call_checker import PatchCallChecker
@@ -22,10 +17,18 @@ class ObjectPatchCallChecker(PatchCallChecker):
             (target_arg := self._target_arg(call)) is not None
             and (attribute_arg := self._attribute_arg(call)) is not None
             and (attribute_value := self._string_value(attribute_arg)) is not None
-            and (original_type := self._attribute_type(target_arg, attribute_value))  # type: ignore
+            and (original_type := self._attribute_type(target_arg, attribute_value)) is not None
         ):
             return self._specialized_patcher_type(original_type)
         return None
 
     def _attribute_arg(self, call: CallExpr) -> Expression | None:
         return ArgMapper.named_arg_mapping(call, self.checker).get("attribute")
+
+    def _attribute_type(self, base: Expression, attribute: str) -> Type | None:
+        type_ = self.checker.expr_checker.analyze_ordinary_member_access(
+            MemberExpr(base, name=attribute), is_lvalue=False
+        )
+        if isinstance(type_, AnyType) and type_.is_from_error:
+            return None
+        return type_
