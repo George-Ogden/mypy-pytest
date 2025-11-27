5664d01286a9312d08d4440d97dc1e5c1c2aa853
George Ogden <george-ogden@outlook.com>
2025-11-27 09:54:17 +0000

    Check whether mark is builtin

--- mypy_pytest_plugin/mark_attributes.py
+++ mypy_pytest_plugin/mark_attributes.py
@@ -1,3 +1,16 @@
+from dataclasses import dataclass
+import functools
+
+from mypy.checker import TypeChecker
+
+
+@dataclass(frozen=True)
 class MarkChecker:
+    checker: TypeChecker
+
     def is_valid_mark(self, name: str) -> bool:
-        return not name.startswith("_")
+        return not name.startswith("_") and name in self.predefined_names
+
+    @functools.cached_property
+    def predefined_names(self) -> set[str]:
+        return set(self.checker.named_type("pytest.MarkGenerator").type.names)
--- mypy_pytest_plugin/mark_attributes_test.py
+++ mypy_pytest_plugin/mark_attributes_test.py
@@ -1,8 +1,22 @@
 from .mark_attributes import MarkChecker
+from .test_utils import parse
 
 
 def _mark_name_test_body(name: str, valid: bool) -> None:
-    mark_checker = MarkChecker()
+    pytest_source_mock = """
+    from typing import Any
+
+    class MarkGenerator:
+        skip: Any
+        parametrize: Any
+        def __getattr__(self, name: str) -> Any:
+            raise NotImplementedError()
+        def _config(self) -> None: ...
+    """
+    parse_result = parse(pytest_source_mock, module_name="pytest")
+    parse_result.accept_all()
+
+    mark_checker = MarkChecker(parse_result.checker)
     assert mark_checker.is_valid_mark(name) == valid
 
 
@@ -10,5 +24,9 @@ def test_mark_name_starts_with_underscore() -> None:
     _mark_name_test_body("_parametrize", False)
 
 
-def test_mark_name_starts_without_underscore() -> None:
+def test_mark_name_valid() -> None:
     _mark_name_test_body("parametrize", True)
+
+
+def test_mark_name_not_predefined() -> None:
+    _mark_name_test_body("invalid", False)
