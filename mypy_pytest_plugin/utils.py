55158f97724ec1ffb8638187a59a29a303e047c1
George Ogden <george-ogden@outlook.com>
2025-12-04 12:44:06 +0000

    Copy across utility fns

--- mypy_pytest_plugin/utils.py
+++ mypy_pytest_plugin/utils.py
@@ -1,4 +1,4 @@
-from collections.abc import Callable, Iterable
+from collections.abc import Callable, Hashable, Iterable
 from typing import Any, overload
 
 
@@ -34,3 +34,36 @@ def strict_not_none[T](expr: T | None, /) -> T:
 def extract_singleton[T](singleton: Iterable[T], /) -> T:
     [value] = singleton
     return value
+
+
+def identity[T](x: T) -> T:
+    return x
+
+
+@overload
+def filter_unique[T: Hashable](it: Iterable[T], key: None = None) -> Iterable[T]: ...
+@overload
+def filter_unique[T, U: Hashable](it: Iterable[T], key: Callable[[T], U]) -> Iterable[T]: ...
+
+
+@overload
+def filter_unique[**P, R: Hashable](
+    fn: Callable[P, Iterable[R]], /
+) -> Callable[P, Iterable[R]]: ...
+
+
+def filter_unique(it: Any, key: None | Callable[[Any], Any] = None) -> Any:
+    if callable(it):
+        return lambda *args, **kwargs: _filter_unique_iterator(it(*args, **kwargs), None)
+    return _filter_unique_iterator(it, key)
+
+
+def _filter_unique_iterator(it: Iterable[Any], key: None | Callable[[Any], Any]) -> Iterable[Any]:
+    seen = set()
+    if key is None:
+        key = identity
+    for element in it:
+        value = key(element)
+        if value not in seen:
+            yield element
+            seen.add(value)
--- mypy_pytest_plugin/utils_test.py
+++ mypy_pytest_plugin/utils_test.py
@@ -1,6 +1,10 @@
+from collections.abc import Callable, Iterable, Sequence
+import itertools
+from typing import Any
+
 import pytest
 
-from .utils import extract_singleton, strict_cast, strict_not_none
+from .utils import extract_singleton, filter_unique, strict_cast, strict_not_none
 
 
 def test_strict_cast_type() -> None:
@@ -45,3 +49,42 @@ def test_strict_not_none_is_none() -> None:
     x: int | None = None
     with pytest.raises(TypeError):
         _: int = strict_not_none(x)
+
+
+@pytest.mark.parametrize(
+    "it, key, expected",
+    [
+        # empty
+        ([], None, []),
+        # already unique
+        (range(10), None, list(range(10))),
+        # already with key
+        (range(10), lambda n: n + 1, list(range(10))),
+        # unique unless key
+        (range(10), lambda n: n // 2, range(0, 10, 2)),
+        # non unique
+        ([1, 2, 3, 3, 2, 5, 1, 3, 0], None, [1, 2, 3, 5, 0]),
+        # non unique with key
+        ([1, 2, -2, -3, -4, 4, 0, -1, 3], lambda x: abs(x), [1, 2, -3, -4, 0]),
+        # infinite unique
+        (itertools.count(), None, list(range(10))),
+        # infinite not unique
+        (itertools.count(), lambda n: n // 3, list(range(0, 20, 3))),
+    ],
+)
+def test_unique_iterator(
+    it: Iterable, key: Callable[[Any], Any] | None, expected: Sequence
+) -> None:
+    assert list(itertools.islice(filter_unique(it, key=key), len(expected))) == list(expected)
+
+
+@filter_unique
+def count_to_n_twice(n: int) -> Iterable[int]:
+    for i in range(n):
+        yield i
+        yield i
+
+
+@pytest.mark.parametrize("n", range(5))
+def test_unique_wrapper(n: int) -> None:
+    assert list(count_to_n_twice(n)) == list(range(n))
