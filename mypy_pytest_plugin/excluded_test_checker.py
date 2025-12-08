2344717d20fe5aa1ac6398313483af4c7479a223
George Ogden <george-ogden@outlook.com>
2025-12-08 14:01:28 +0000

    Check test return types when running plugin

--- mypy_pytest_plugin/excluded_test_checker.py
+++ mypy_pytest_plugin/excluded_test_checker.py
@@ -1,4 +1,5 @@
 from collections.abc import Iterable, Sequence
+from dataclasses import dataclass
 import itertools
 
 from mypy.checker import TypeChecker
@@ -12,44 +13,47 @@ from mypy.nodes import (
 from mypy.subtypes import is_same_type
 from mypy.types import LiteralType
 
+from .checker_wrapper import CheckerWrapper
 from .defer import DeferralError, DeferralReason
+from .fullname import Fullname
+from .test_name_checker import TestNameChecker
 
 
-class ExcludedTestChecker:
+@dataclass(frozen=True, eq=False)
+class ExcludedTestChecker(CheckerWrapper):
+    checker: TypeChecker
+
     @classmethod
-    def ignored_test_names(cls, defs: Sequence[Statement], checker: TypeChecker) -> set[str]:
-        return cls._ignored_test_names_from_statements(defs, checker)
+    def is_test(cls, fullname: str, checker: TypeChecker) -> bool:
+        ignored_testnames = cls(checker).ignored_test_names(checker.tree.defs)
+        return Fullname.from_string(
+            fullname
+        ).name not in ignored_testnames and TestNameChecker.is_test_name(fullname)
 
-    @classmethod
-    def _ignored_test_names_from_statements(
-        cls, statements: Sequence[Statement], checker: TypeChecker
-    ) -> set[str]:
-        return cls._ignored_test_names_from_assignments(
+    def ignored_test_names(self, defs: Sequence[Statement]) -> set[str]:
+        return self._ignored_test_names_from_statements(defs)
+
+    def _ignored_test_names_from_statements(self, statements: Sequence[Statement]) -> set[str]:
+        return self._ignored_test_names_from_assignments(
             [statement for statement in statements if isinstance(statement, AssignmentStmt)],
-            checker=checker,
         )
 
-    @classmethod
     def _ignored_test_names_from_assignments(
-        cls, assignments: Sequence[AssignmentStmt], checker: TypeChecker
+        self, assignments: Sequence[AssignmentStmt]
     ) -> set[str]:
         return set(
             itertools.chain.from_iterable(
-                cls._identify_non_test_assignment_names(assignment, checker)
-                for assignment in assignments
+                self._identify_non_test_assignment_names(assignment) for assignment in assignments
             )
         )
 
-    @classmethod
-    def _identify_non_test_assignment_names(
-        cls, assignment: AssignmentStmt, checker: TypeChecker
-    ) -> Iterable[str]:
-        rvalue_type = checker.lookup_type_or_none(assignment.rvalue)
+    def _identify_non_test_assignment_names(self, assignment: AssignmentStmt) -> Iterable[str]:
+        rvalue_type = self.checker.lookup_type_or_none(assignment.rvalue)
         if rvalue_type is None:
             raise DeferralError(DeferralReason.REQUIRED_WAIT)
-        if is_same_type(rvalue_type, LiteralType(False, checker.named_type("builtins.bool"))):
+        if is_same_type(rvalue_type, LiteralType(False, self.checker.named_type("builtins.bool"))):
             for lvalue in assignment.lvalues:
-                assignment_target = cls._test_assignment_target(lvalue)
+                assignment_target = self._test_assignment_target(lvalue)
                 if assignment_target is not None:
                     yield assignment_target
 
