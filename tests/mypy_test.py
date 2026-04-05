51ac3938dbbdf577dca9174e987cbb81c8cfb253
George Ogden <george-ogden@outlook.com>
2026-04-05 16:19:33 +0100

    Ignore redundant casts during testing

--- tests/mypy_test.py
+++ tests/mypy_test.py
@@ -15,7 +15,7 @@ TEST_FILES = list(filter(Path.exists, map(Path, glob("test_samples/**/*.py", rec
 def test_check_files(filepath: Path) -> None:
     sys.modules.pop("plugin", None)  # required for plugin to work correctly
     stdout, stderr, _exit_code = mypy.api.run(
-        [str(filepath), "--cache-dir", os.devnull, "--show-traceback"]
+        [str(filepath), "--cache-dir", os.devnull, "--show-traceback", "--no-warn-redundant-casts"]
     )
     stdout = stdout.replace(os.getcwd(), "[CWD]")
     assert stderr == "", stderr
